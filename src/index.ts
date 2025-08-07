d6e3c81e9c21f84dd69c6479857ddeeacba91088
Raku Zeta <zetaraku@gmail.com>
2025-08-07 23:16:11 +0800

    ♻️ extract environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import { COSCUP_YEAR, makeData } from './util';
+import { makeData } from './util';
 import type { RawData } from './types/raw-data';
 import type { Data } from './types/data';
 
-export async function getRawData(): Promise<RawData> {
-  const rawDataUrl = `https://coscup.org/${COSCUP_YEAR}/json/session.json`;
+export async function getRawData(year: string): Promise<RawData> {
+  const rawDataUrl = `https://coscup.org/${year}/json/session.json`;
   const rawData = await fetch(rawDataUrl).then((res) => res.json()) as RawData;
 
   return rawData;
 }
 
-export async function getData(): Promise<Data> {
-  const rawData = await getRawData();
-  const data = makeData(rawData);
+export async function getData(year: string, days: string[], timezone: string): Promise<Data> {
+  const rawData = await getRawData(year);
+  const data = makeData(rawData, year, days, timezone);
 
   return data;
 }
 
 async function main() {
-  const rawData = await getRawData();
+  const COSCUP_YEAR = process.env.COSCUP_YEAR ?? '2077';
+  const COSCUP_DAYS = JSON.parse(process.env.COSCUP_DAYS ?? '[]') as string[];
+  const COSCUP_TIMEZONE = process.env.COSCUP_TIMEZONE ?? '+08:00';
+
+  const rawData = await getRawData(COSCUP_YEAR);
 
   fs.writeFileSync(
     path.join(__dirname, '../data/raw-data.json'),
     JSON.stringify(rawData, null, '\t'),
   );
 
-  const data = makeData(rawData);
+  const data = makeData(rawData, COSCUP_YEAR, COSCUP_DAYS, COSCUP_TIMEZONE);
 
   fs.writeFileSync(
     path.join(__dirname, '../data/data.json'),
--- src/util.ts
+++ src/util.ts
@@ -2,50 +2,54 @@ import * as Luxon from 'luxon';
 import type { RawData } from './types/raw-data';
 import type { Data, College, Department, Course } from './types/data';
 
-export const COSCUP_YEAR = process.env.COSCUP_YEAR ?? '2077';
-export const COSCUP_DAYS = JSON.parse(process.env.COSCUP_DAYS ?? '[]') as string[];
-export const COSCUP_TIMEZONE = process.env.COSCUP_TIMEZONE ?? '+08:00';
+function buildGetClassTimes(days: string[], timezone: string) {
+  const DAYS = days.map((day, index) => ({
+    key: `${1 + index}`,
+    name: `Day ${1 + index}`,
+    date: day,
+  }));
 
-export const DAYS = COSCUP_DAYS.map((day, index) => ({
-  key: `${1 + index}`,
-  name: `Day ${1 + index}`,
-  date: day,
-}));
+  const HOURS = [
+    { key: '1', name: '1', start: '08:00:00', end: '09:00:00' },
+    { key: '2', name: '2', start: '09:00:00', end: '10:00:00' },
+    { key: '3', name: '3', start: '10:00:00', end: '11:00:00' },
+    { key: '4', name: '4', start: '11:00:00', end: '12:00:00' },
+    { key: 'Z', name: 'Z', start: '12:00:00', end: '13:00:00' },
+    { key: '5', name: '5', start: '13:00:00', end: '14:00:00' },
+    { key: '6', name: '6', start: '14:00:00', end: '15:00:00' },
+    { key: '7', name: '7', start: '15:00:00', end: '16:00:00' },
+    { key: '8', name: '8', start: '16:00:00', end: '17:00:00' },
+    { key: '9', name: '9', start: '17:00:00', end: '18:00:00' },
+    { key: 'A', name: 'A', start: '18:00:00', end: '19:00:00' },
+    { key: 'B', name: 'B', start: '19:00:00', end: '20:00:00' },
+    { key: 'C', name: 'C', start: '20:00:00', end: '21:00:00' },
+    { key: 'D', name: 'D', start: '21:00:00', end: '22:00:00' },
+  ];
 
-export const HOURS = [
-  { key: '1', name: '1', start: '08:00:00', end: '09:00:00' },
-  { key: '2', name: '2', start: '09:00:00', end: '10:00:00' },
-  { key: '3', name: '3', start: '10:00:00', end: '11:00:00' },
-  { key: '4', name: '4', start: '11:00:00', end: '12:00:00' },
-  { key: 'Z', name: 'Z', start: '12:00:00', end: '13:00:00' },
-  { key: '5', name: '5', start: '13:00:00', end: '14:00:00' },
-  { key: '6', name: '6', start: '14:00:00', end: '15:00:00' },
-  { key: '7', name: '7', start: '15:00:00', end: '16:00:00' },
-  { key: '8', name: '8', start: '16:00:00', end: '17:00:00' },
-  { key: '9', name: '9', start: '17:00:00', end: '18:00:00' },
-  { key: 'A', name: 'A', start: '18:00:00', end: '19:00:00' },
-  { key: 'B', name: 'B', start: '19:00:00', end: '20:00:00' },
-  { key: 'C', name: 'C', start: '20:00:00', end: '21:00:00' },
-  { key: 'D', name: 'D', start: '21:00:00', end: '22:00:00' },
-];
+  const DAY_HOURS = DAYS.flatMap(
+    (day, i) => HOURS.map(
+      (hour, j) => ({
+        i, j,
+        day, hour,
+        key: `${day.key}-${hour.key}`,
+        interval: Luxon.Interval.fromDateTimes(
+          Luxon.DateTime.fromISO(`${day.date}T${hour.start}${timezone}`),
+          Luxon.DateTime.fromISO(`${day.date}T${hour.end}${timezone}`),
+        ),
+      }),
+    ),
+  );
 
-export const DAY_HOURS = DAYS.flatMap(
-  (day, i) => HOURS.map(
-    (hour, j) => ({
-      i, j,
-      day, hour,
-      key: `${day.key}-${hour.key}`,
-      interval: Luxon.Interval.fromDateTimes(
-        Luxon.DateTime.fromISO(`${day.date}T${hour.start}${COSCUP_TIMEZONE}`),
-        Luxon.DateTime.fromISO(`${day.date}T${hour.end}${COSCUP_TIMEZONE}`),
-      ),
-    }),
-  ),
-);
+  return function classTimeTransformer(interval: Luxon.Interval): string[] {
+    return DAY_HOURS.filter((dayHour) => dayHour.interval.overlaps(interval)).map((dayHour) => dayHour.key);
+  };
+}
 
-export function makeData(rawData: RawData): Data {
-  const defaultCollegeId = `coscup-${COSCUP_YEAR}`;
-  const defaultCollegeName = `COSCUP ${COSCUP_YEAR}`;
+export function makeData(rawData: RawData, year: string, days: string[], timezone: string): Data {
+  const getClassTimes = buildGetClassTimes(days, timezone);
+
+  const defaultCollegeId = `coscup-${year}`;
+  const defaultCollegeName = `COSCUP ${year}`;
 
   const colleges: College[] = [
     {
@@ -73,7 +77,7 @@ export function makeData(rawData: RawData): Data {
       credit: interval.toDuration().as('minutes'),
       passwordCard: 'NONE',
       teachers: session.speakers.map((speakerId) => rawData.speakers.find((speaker) => speaker.id === speakerId)!.zh.name),
-      classTimes: DAY_HOURS.filter((dayHour) => dayHour.interval.overlaps(interval)).map((dayHour) => dayHour.key),
+      classTimes: getClassTimes(interval),
       limitCnt: null,
       admitCnt: 0,
       waitCnt: 0,
